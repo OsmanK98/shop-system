0048a8ff09ceb3b289b62deb60980175762c5cea
Krzysztof <kontakt.osman@gmail.com>
2021-06-13 17:22:53 +0200

    Configure docker environment

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,7 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
